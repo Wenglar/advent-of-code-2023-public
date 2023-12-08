1acb65322deb3b221e749a7b839c78c1d16e5df1
Vojtech <161917@vutbr.cz>
2023-12-08 20:37:23 +0100

    Update Day 06 part 2: add non-brute-force solution

--- day_06/boats_part2.py
+++ day_06/boats_part2.py
@@ -48,17 +48,32 @@ with open(FILEPATH, encoding='utf-8') as input_:
 # end = time.perf_counter()
 
 # Time: 1.5s
-start = time.perf_counter()
-for t in range(boat.time):
-    d = t * (boat.time - t)
-    if d > boat.distance:
-        if boat.time % 2 == 0:
-            result = ((boat.time // 2) - t) * 2 + 1
-        else:
-            result = ((boat.time // 2) - t + 1) * 2
-        break
-end = time.perf_counter()
+# start = time.perf_counter()
+# for t in range(boat.time):
+#     d = t * (boat.time - t)
+#     if d > boat.distance:
+#         if boat.time % 2 == 0:
+#             result = ((boat.time // 2) - t) * 2 + 1
+#         else:
+#             result = ((boat.time // 2) - t + 1) * 2
+#         break
+# end = time.perf_counter()
 
+# Time: cca 0.000013s
+start = time.perf_counter()
+
+lim = math.floor((boat.time / 2) ** 2)
+dif = lim - boat.distance
+
+if dif > 0:
+    if boat.time % 2 == 0:
+        x = math.sqrt(abs(dif - 0.01))
+        result = 2 * (math.ceil(x) - 1) + 1
+    else:
+        x = 0.5 * (math.sqrt(4 * dif + 1) - 1)
+        result = 2 * math.ceil(x)
+
+end = time.perf_counter()
 
 print(result)
 print(result == 35349468)
